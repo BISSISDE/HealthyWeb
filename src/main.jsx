9df320769e15e71dc3e2cc1d71e0eb0d34378349
Nurkadur Aiganym <ajganymnrkadyr@gmail.com>
2025-05-16 16:28:19 +0500

    react project

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { AuthProvider } from "./context/AuthContext";
 import "./index.css";
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <AuthProvider>
-    <App />
+      <App />
   </AuthProvider>
 );
